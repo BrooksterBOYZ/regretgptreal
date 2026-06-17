02394c85f2cfb73ef60f31c5ddb40f5531d5e767
Brooks Tafreshi <brtafreshi2023@gmail.com>
2026-06-17 09:36:14 -0500

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', 
+  images: {
+    unoptimized: true, 
+  },
+  eslint: {
+    // This allows the production build to successfully complete 
+    // even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // This allows the production build to successfully complete
+    // even if your project has TypeScript type errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
